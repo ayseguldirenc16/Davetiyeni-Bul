8f0bceb1a37d9506f2fa19b8580f5c7e68d4efdd
Ayşegül Direnç <diren---162@hotmail.com>
2024-07-28 19:04:43 +0300

    The design part is completed

--- src/body/Head.jsx
+++ src/body/Head.jsx
@@ -1,47 +1,37 @@
 import './body.css';
 
-function Head (){
-    return(
+function Head() {
+    return (
         <>
-        <nav className="navbar navbar-expand-lg ">
-  <div className="container-fluid">  
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link " aria-current="page" href="#">Anasayfa</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Çalışmalarımız</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Daha Fazla
-          </a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="#">Giriş Yap</a></li>
-            <li><a className="dropdown-item" href="#">Üye ol</a></li>
-            <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="#">Sipariş Takibi</a></li>
-          </ul>
-        </li>
-       
-      </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav> 
-
-
-
-
+            <nav className="navbar sticky-top">
+                <div className="container-fluid">
+                    <img src="images/logo.jpg" alt="" className='navbar-img' />
+                    <div className="navbar-text">
+                        <div className="links">
+                            <a href="#">Anasayfa</a>
+                            <a href="#">Hakkımızda</a>
+                            <a href="#">Kampanyalar</a>
+                            <a href="#">İletişim</a>
+                        </div>
+                        <div className="dropdown">
+                            <button className="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Üyelik
+                            </button>
+                            <ul className="dropdown-menu">
+                                <li><a className="dropdown-item" href="#">Üye Ol</a></li>
+                                <li><a className="dropdown-item" href="#">Giriş Yap</a></li>
+                                <li><a className="dropdown-item" href="#">Sipariş Takibi</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <form className="d-flex" role="search">
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                        <button className="btn btn-outline-success " type="submit">Search</button>
+                    </form>
+                </div>
+            </nav>
         </>
-    )
+    );
 }
 
-export default Head
+export default Head;
